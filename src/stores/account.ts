fee2bdac14d03fd1a6a0cc3ba76b6116db973a68
FarhodD <farhoddavlatov0120@gmail.com>
2025-02-25 10:36:55 +0500

    feat: хранение данных в localStorage

--- src/stores/account.ts
+++ src/stores/account.ts
@@ -1,4 +1,4 @@
-import { ref, computed } from 'vue'
+import { ref, computed, watch } from 'vue'
 import { defineStore } from 'pinia'
 interface Account {
   id: number;
@@ -9,7 +9,16 @@ interface Account {
 }
 
 export const useAccountStore = defineStore('account', () => {
-  const accounts = ref<Account[]>([])
+  const loadAccounts = (): Account[] => {
+    const accountsData = localStorage.getItem('accounts')
+    return accountsData ? JSON.parse(accountsData) : []
+  }
+  
+  const accounts = ref<Account[]>(loadAccounts())
+
+  const saveAccounts = () => {
+    localStorage.setItem('accounts', JSON.stringify(accounts.value))
+  }
 
   const addAccount = () => {
     accounts.value.push({
@@ -19,23 +28,28 @@ export const useAccountStore = defineStore('account', () => {
       login: '',
       password: ''
     })
+    saveAccounts();
     console.log('ADDED ACCOUNT: ', accounts.value);
   }
 
   const removeAccount = (index: number) => {
     console.log('DELETE ACCOUNT ID: ', index)
     accounts.value.splice(index, 1)
+    saveAccounts();
   }
 
   const updateAccount = (index: number, field: keyof Account, value: any) => {
     if (accounts.value[index]) {
       (accounts.value[index] as any)[field] = value;
+      saveAccounts();
     }
   };
 
   const labelString = computed(() =>
     accounts.value.map(account => account.labels.map(label => label.text).join('; '))
   );
+
+  watch(accounts, saveAccounts, { deep: true });
 
   return { accounts, addAccount, removeAccount, updateAccount, labelString }
 
